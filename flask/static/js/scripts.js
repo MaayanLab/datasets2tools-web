ee5f832b9f2c78784956472f6f5c3f0782a91cbb
denis-torre <denis.torre@mssm.edu>
2017-04-25 17:08:01 -0400

    Refactoring

--- flask/static/js/scripts.js
+++ flask/static/js/scripts.js
@@ -1,3 +1,9 @@
+////////////////////////////
+////////// Search //////////
+////////////////////////////
+
+////////// 1. Object Button Listener
+
 $('input[type="radio"]').click(function(evt) {
 	var objects = $(evt.target).attr('id');
 
@@ -10,72 +16,65 @@ $('input[type="radio"]').click(function(evt) {
 	}
 
 	$('.search-example-link').click(function(evt) {
-		// var example_tags = '';
-		var $tags_input = $('#keywords-label').find('.tags-input');
+		var $tags_input = $('#keyword-input-row').find('.tags-input');
 		$tags_input.find('.tag').remove();
 		$.each($(evt.target).text().split(', ').reverse(), function(index, value) {
 			$tags_input.prepend('<span class="tag" data-tag="'+value+'">'+value+'</span>')
 		})
 		$('#search-button').click()
-	});
+	})
 });
 
 $('#analyses').click();
 
+////////// 2. Search Button Click Listener
+
 $('#search-button').click(function(evt) {
 
 	var objects = $('input[type="radio"][name="radio"]:checked').attr('id');
-
 	var keywords = [];
-
 	$('.tags-input').find('span').each(function(i, elem) {
 		keywords.push($(elem).text());
 	});
 
-	var alertdiv = '<div class="form-alert justify-content-center"><i class="fa fa-exclamation-circle fa-2x" aria-hidden="true"></i>Please fill in the following information.</div>'
-
 	if (keywords.length === 0) {
-		$('#keyword-input-text .form-alert').hide();
-		$('#keyword-input-text').prepend(alertdiv);
-	}
+		console.log('asd');
+	} else {
 
-	if (objects.length == 0 && keywords.length > 0) {
-		$('#keyword-input-text .form-alert').hide();
-	}
+	$.ajax({
+	url: 'http://localhost:5000/datasets2tools/keyword_search',
+	data: {
+	  'obj': objects,
+	  'keywords': keywords.join(','),
+	},
 
-	if (objects.length > 0 && keywords.length > 0) {
-		$('.form-alert').hide();
-		// var alertstring = 'You are searching <strong>' + objects.join('</strong>, <strong>') + '</strong> for the following keywords: <strong>' + keywords.join('</strong>, <strong>') + '</strong>.';
-		  $.ajax({
-		    url: 'http://localhost:5000/datasets2tools/keyword_search',
-		    data: {
-		      'obj': objects,
-		      'keywords': keywords.join(','),
-		    },
-		    success: function(data) {
-		    	$("#search-results").html(data);
-				$(function() {$('[data-toggle="tooltip"]').tooltip()});
-		    },
-		    error: function() {
-		    	$("#search-results").html('Sorry, no search results found for the specified query.');
-		    }
-		  });
-		setTimeout(function() {
-			$("#results_container").show();
-			$('html, body').animate({
-			        scrollTop: $("#results_container").offset().top - $('#navbar').height()
-			    }, 750);	
-		}, 500)
+	success: function(data) {
+		$("#search-results").html(data);
+		$(function() {$('[data-toggle="tooltip"]').tooltip()});
+	},
+
+	error: function() {
+		$("#search-results").html('Sorry, there was an error.');
 	}
+	});
+
+	setTimeout(function() {
+		$("#search-results-wrapper").show();
+		$('html, body').animate({
+		        scrollTop: $("#search-results-wrapper").offset().top
+		    }, 750);	
+	}, 750)
+}
+
 });
 
 $('#back-button').click(function(evt) {
 	setTimeout(function(){
-		$("#results_container").hide();
+		$("#search-results-wrapper").hide();
 		$("#search-results").html('');
 	}, 800);
 	$('html, body').animate({
-        scrollTop: $("#main_container").offset().top 
+        scrollTop: $("#main-container").offset().top 
     }, 750);
 });
 
